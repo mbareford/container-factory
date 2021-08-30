6888c4d0fe6cd0de7e9d2d419f40d7d6cd5d2f9f
mbareford <m.bareford@epcc.ed.ac.uk>
2021-08-30 10:19:23 +0000

    Fixed GROMACS Cirrus build env.

--- scripts/app/gromacs/host/cirrus/impi19-ib/gcc10/env.sh
+++ scripts/app/gromacs/host/cirrus/impi19-ib/gcc10/env.sh
@@ -1,6 +1,4 @@
 MPI_ROOT=/opt/sw/intel/compilers_and_libraries_2019.0.117/linux/mpi/intel64
-MPI_C_LIB=mpicxx
-MPI_CXX_LIB=mpicxx
 
 FFTW_ROOT=/opt/sw/fftw/3.3.9-mpt2-gcc8
 
@@ -9,3 +7,5 @@ BLAS_LIBRARIES=${MKL_ROOT}/lib/intel64_lin
 LAPACK_LIBRARIES=${BLAS_LIBRARIES}
 
 LD_LIBRARY_PATH=${FFTW_ROOT}/lib:${MKL_ROOT}/lib/intel64_lin:${MPI_ROOT}/lib:${MPI_ROOT}/lib/release:${MPI_ROOT}/libfabric/lib:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu/libibverbs:/.singularity.d/libs
+
+PATH=${MPI_ROOT}/bin:${PATH}
--- scripts/app/gromacs/host/cirrus/mpt2-ib/gcc10/env.sh
+++ scripts/app/gromacs/host/cirrus/mpt2-ib/gcc10/env.sh
@@ -1,6 +1,4 @@
 MPI_ROOT=/opt/hpe/hpc/mpt/mpt-2.22
-MPI_C_LIB=mpi
-MPI_CXX_LIB=mpi++
 
 FFTW_ROOT=/opt/sw/fftw/3.3.9-mpt2-gcc8
 
@@ -9,3 +7,5 @@ BLAS_LIBRARIES=${MKL_ROOT}/lib/intel64_lin
 LAPACK_LIBRARIES=${BLAS_LIBRARIES}
 
 LD_LIBRARY_PATH=${FFTW_ROOT}/lib:${MKL_ROOT}/lib/intel64_lin:${MPI_ROOT}/lib:/opt/sw/pmi2/lib:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu/libibverbs:/.singularity.d/libs
+
+PATH=${MPI_ROOT}/bin:${PATH}
