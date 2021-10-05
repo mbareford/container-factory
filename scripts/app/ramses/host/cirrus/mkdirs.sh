fa18debb87b2e25f340c586fa211fa2b7922a0d0
mbareford <m.bareford@epcc.ed.ac.uk>
2021-10-05 17:15:53 +0000

    Revised add_dirs aux script.

--- scripts/app/ramses/host/cirrus/mkdirs.sh
+++ scripts/app/ramses/host/cirrus/mkdirs.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
   
 declare -a hostpaths=("/lustre/sw" "/opt/hpe" "/etc/libibverbs.d")
-
-HOST=Cirrus
-PATH_LOG=/opt/hostpath.log
-
-if [ ! -f ${PATH_LOG} ]; then
-  touch ${PATH_LOG}
-fi
-
-for hpath in "${hostpaths[@]}"; do
-  mkdir -p ${hpath}
-  LOG_LINE="${HOST}: ${hpath}"
-  if ! grep -q "${LOG_LINE}" ${PATH_LOG}; then
-    echo "${LOG_LINE}" >> ${PATH_LOG}
-  fi
-done
