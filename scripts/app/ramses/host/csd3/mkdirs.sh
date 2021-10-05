205116de8face623e19ac81e24c836a492158b07
mbareford <m.bareford@epcc.ed.ac.uk>
2021-10-05 17:16:09 +0000

    Revised add_dirs aux script.

--- scripts/app/ramses/host/csd3/mkdirs.sh
+++ scripts/app/ramses/host/csd3/mkdirs.sh
@@ -5,18 +5,3 @@ declare -a hostpaths=("/scratch" "/lustre1" "/lustre2" "/lustre3" "/lustre4" "/l
     "/rds-d1" "/rds-d2" "/rds-d3" "/rds-d4" "/rds-d5" "/rds" \
     "/rcs1" "/rcs2" "/rcs3" "/rcs" "/rfs" "/local" "/ramdisks" \
     "/usr/local/Cluster-Apps" )
-
-HOST=CSD3
-PATH_LOG=/opt/hostpath.log
-
-if [ ! -f ${PATH_LOG} ]; then
-  touch ${PATH_LOG}
-fi
-
-for hpath in "${hostpaths[@]}"; do
-  mkdir -p ${hpath}
-  LOG_LINE="${HOST}: ${hpath}"
-  if ! grep -q "${LOG_LINE}" ${PATH_LOG}; then
-    echo "${LOG_LINE}" >> ${PATH_LOG}
-  fi
-done
