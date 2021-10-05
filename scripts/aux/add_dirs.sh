f1e8b73928e2a995a9bb7188395b4c5f5abae54d
mbareford <m.bareford@epcc.ed.ac.uk>
2021-10-05 17:13:50 +0000

    Revised add_dirs aux script.

--- scripts/aux/add_dirs.sh
+++ scripts/aux/add_dirs.sh
@@ -7,6 +7,25 @@ APP=$1
 HOST=$2
 SCRIPT_PATH=/opt/scripts/app/${APP}/host/${HOST}
 
+PATH_LOG=/opt/logs/hostpath.log
+if [ ! -f ${PATH_LOG} ]; then
+  touch ${PATH_LOG}
+fi
+
 if [ -f ${SCRIPT_PATH}/mkdirs.sh ]; then
-  ${SCRIPT_PATH}/mkdirs.sh
+  source ${SCRIPT_PATH}/mkdirs.sh
+
+  if [ ! -f ${PATH_LOG} ]; then
+    touch ${PATH_LOG}
+  fi
+
+  for hpath in "${hostpaths[@]}"; do
+    mkdir -p ${hpath}
+    if [ -d ${hpath} ]; then
+      LOG_LINE="${HOST}: ${hpath}"
+      if ! grep -q "${LOG_LINE}" ${PATH_LOG}; then
+        echo "${LOG_LINE}" >> ${PATH_LOG}
+      fi
+    fi
+  done
 fi
