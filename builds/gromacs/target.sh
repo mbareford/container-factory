23a4fa6086ccf8312170d72bc10463e487b6db52
mbareford <m.bareford@epcc.ed.ac.uk>
2021-10-19 14:44:30 +0000

    updated gromacs and ramses target scripts

--- builds/gromacs/target.sh
+++ builds/gromacs/target.sh
@@ -6,8 +6,9 @@ SCRIPTS_ROOT=${HOME}/work/scripts
 SCRIPTS_SNG=${SCRIPTS_ROOT}/fac/singularity
 
 
-${SCRIPTS_SNG}/target.sh ${SCRIPTS_ROOT} ${PWD} ${APP} archer2 "/work/z19/z19/mrb4cab/containers/${APP}" "2021.1 cmpich8-ofi gcc10"
-${SCRIPTS_SNG}/target.sh ${SCRIPTS_ROOT} ${PWD} ${APP} archer2 "/work/z19/z19/mrb4cab/containers/${APP}" "2021.1 ompi4-ofi gcc10"
+${SCRIPTS_SNG}/target_init.sh ${SCRIPTS_ROOT} ${PWD} ${APP} archer2
+${SCRIPTS_SNG}/target.sh ${SCRIPTS_ROOT} ${PWD} ${APP} archer2 "/work/z19/z19/mrb23cab/containers/${APP}" "2021.1 cmpich8-ofi gcc10"
+${SCRIPTS_SNG}/target.sh ${SCRIPTS_ROOT} ${PWD} ${APP} archer2 "/work/z19/z19/mrb23cab/containers/${APP}" "2021.1 ompi4-ofi gcc10"
 
 ${SCRIPTS_SNG}/target_init.sh ${SCRIPTS_ROOT} ${PWD} ${APP} cirrus
 ${SCRIPTS_SNG}/target.sh ${SCRIPTS_ROOT} ${PWD} ${APP} cirrus "/lustre/home/z04/mrb/containers/${APP}" "2021.1 mpt2-ib gcc10"
