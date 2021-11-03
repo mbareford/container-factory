a91ff0441df9fd7028f2e934820d611a3d1a99ed
mbareford <m.bareford@epcc.ed.ac.uk>
2021-11-03 15:31:41 +0000

    Added archer2-cab targeting comand.

--- builds/nemo/target.sh
+++ builds/nemo/target.sh
@@ -9,5 +9,7 @@ SCRIPTS_SNG=${SCRIPTS_ROOT}/fac/singularity
 ${SCRIPTS_SNG}/target_init.sh ${SCRIPTS_ROOT} ${PWD} ${APP} archer2
 ${SCRIPTS_SNG}/target.sh ${SCRIPTS_ROOT} ${PWD} ${APP} archer2 "/work/z19/z19/mrb23cab/containers/${APP}" "2.5 4.0.6 GYRE_PISCES GYRE_PISCES_CFG cmpich8 gcc10"
 
+${SCRIPTS_SNG}/target.sh ${SCRIPTS_ROOT} ${PWD} ${APP} archer2-4cab "/work/z19/z19/mrb4cab/containers/${APP}" "2.5 4.0.6 GYRE_PISCES GYRE_PISCES_CFG cmpich8 gcc10"
+
 ${SCRIPTS_SNG}/target_init.sh ${SCRIPTS_ROOT} ${PWD} ${APP} cirrus
 ${SCRIPTS_SNG}/target.sh ${SCRIPTS_ROOT} ${PWD} ${APP} cirrus "/lustre/home/z04/mrb/containers/${APP}" "2.5 4.0.6 GYRE_PISCES GYRE_PISCES_CFG mpt2-ib gcc10"
