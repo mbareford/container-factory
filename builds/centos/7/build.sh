d1440c336b19eb7733117a863d4499fc7cd4d85f
mbareford <m.bareford@epcc.ed.ac.uk>
2021-11-03 15:17:16 +0000

    Updated Centos7 build script.

--- builds/centos/7/build.sh
+++ builds/centos/7/build.sh
@@ -3,4 +3,7 @@
 rm -f *.sif
 rm -f *.out
 
-$HOME/scripts/fac/singularity/build_container.sh centos7
+SCRIPTS_ROOT=${HOME}/work/scripts
+SCRIPTS_DEF=${SCRIPTS_ROOT}/def
+
+sudo singularity build ${PWD}/centos7.sif ${SCRIPTS_DEF}/centos7.def
